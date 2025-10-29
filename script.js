fa30319d2147d3946aaac560415f57c55fe329e6
jass1509 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-10-29 11:51:42 -0500

    firt modification

---
+++ script.js
@@ -0,0 +1,8 @@
+//ejercicio 3 doblar numero
+function   prog3() {
+    let array=prompt("Ingrese una serie de números separados por comas:").split(",").map(Number);
+array = array * 2
+        alert(array);
+    }
+
+    
